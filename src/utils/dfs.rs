f9ae3c7a6b146fb1c2e452c342535bdeefa1ade4
Lukasz Szpakowski <luckboy@vp.pl>
2024-04-15 17:02:50 +0200

    Changed something.

--- src/utils/dfs.rs
+++ src/utils/dfs.rs
@@ -8,14 +8,14 @@
 use std::collections::BTreeSet;
 
 pub fn dfs_with_error<T: Clone + Ord, U, E, F, G>(from: &T, data: &mut U, mut f: F, mut g: G) -> Result<(), E>
-    where F: FnMut(&T, &mut U, &BTreeSet<T>) -> Result<Vec<T>, E>,
+    where F: FnMut(&T, &BTreeSet<T>, &mut U) -> Result<Vec<T>, E>,
         G: FnMut(&T, &mut U) -> Result<(), E>
 {
     let mut stack: Vec<(T, Vec<T>)> = Vec::new();
     let mut visited: BTreeSet<T> = BTreeSet::new();
     let mut processed: BTreeSet<T> = BTreeSet::new();
     processed.insert(from.clone());
-    let mut tmp_neighbors = f(from, data, &processed)?;
+    let mut tmp_neighbors = f(from, &processed, data)?;
     tmp_neighbors.reverse();
     stack.push((from.clone(), tmp_neighbors));
     visited.insert(from.clone());
@@ -35,7 +35,7 @@ pub fn dfs_with_error<T: Clone + Ord, U, E, F, G>(from: &T, data: &mut U, mut f:
                         processed.insert(u.clone());
                         stack.push((u, neighbors));
                         processed.insert(v.clone());
-                        let mut tmp_neighbors = f(&v, data, &processed)?;
+                        let mut tmp_neighbors = f(&v, &processed, data)?;
                         tmp_neighbors.reverse();
                         stack.push((v.clone(), tmp_neighbors));
                         visited.insert(v.clone());
@@ -50,6 +50,6 @@ pub fn dfs_with_error<T: Clone + Ord, U, E, F, G>(from: &T, data: &mut U, mut f:
 }
 
 pub fn dfs<T: Clone + Ord, U, F, G>(from: &T, data: &mut U, mut f: F, mut g: G)
-    where F: FnMut(&T, &mut U, &BTreeSet<T>) -> Vec<T>,
+    where F: FnMut(&T, &BTreeSet<T>, &mut U) -> Vec<T>,
         G: FnMut(&T, &mut U)
-{ let _res: Result<(), ()> = dfs_with_error(from, data, |u, data, processed| Ok(f(u, data, processed)), |u, data| Ok(g(u, data))); }
+{ let _res: Result<(), ()> = dfs_with_error(from, data, |u, processed, data| Ok(f(u, processed, data)), |u, data| Ok(g(u, data))); }
