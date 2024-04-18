28cdcc9564c747eb4b294433084c39ef75059f7a
Lukasz Szpakowski <luckboy@vp.pl>
2024-04-18 14:20:27 +0200

    Amended code.

--- src/frontend/namer.rs
+++ src/frontend/namer.rs
@@ -5,8 +5,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
-use std::collections::BTreeSet;
 use std::cell::*;
+use std::collections::BTreeSet;
 use std::rc::*;
 use crate::frontend::error::*;
 use crate::frontend::tree::*;
