8783ee169297b2a1def124cd0d5ea5c5aab7a6e0
Lukasz Szpakowski <luckboy@vp.pl>
2024-04-25 19:44:56 +0200

    Refactoring.

--- src/utils/dfs.rs
+++ src/utils/dfs.rs
@@ -7,40 +7,40 @@
 //
 use std::collections::BTreeSet;
 
-pub fn dfs_with_result<T: Clone + Ord, U, E, F, G>(from: &T, visited: &mut BTreeSet<T>, data: &mut U, mut f: F, mut g: G) -> Result<(), E>
+pub fn dfs_with_result<T: Clone + Ord, U, E, F, G>(from: &T, visiteds: &mut BTreeSet<T>, data: &mut U, mut f: F, mut g: G) -> Result<(), E>
     where F: FnMut(&T, &BTreeSet<T>, &mut U) -> Result<Vec<T>, E>,
         G: FnMut(&T, &mut U) -> Result<(), E>
 {
     let mut stack: Vec<(T, Vec<T>)> = Vec::new();
-    let mut processed: BTreeSet<T> = BTreeSet::new();
-    if visited.contains(from) {
+    let mut processeds: BTreeSet<T> = BTreeSet::new();
+    if visiteds.contains(from) {
         return Ok(());
     }
-    processed.insert(from.clone());
-    let mut tmp_neighbors = f(from, &processed, data)?;
+    processeds.insert(from.clone());
+    let mut tmp_neighbors = f(from, &processeds, data)?;
     tmp_neighbors.reverse();
     stack.push((from.clone(), tmp_neighbors));
-    visited.insert(from.clone());
+    visiteds.insert(from.clone());
     loop {
         match stack.pop() {
             Some((u, mut neighbors)) => {
-                processed.remove(&u);
+                processeds.remove(&u);
                 let v = loop {
                     match neighbors.pop() {
-                        Some(w) if visited.contains(&w) => (),
+                        Some(w) if visiteds.contains(&w) => (),
                         Some(w) => break Some(w),
                         None => break None,
                     }
                 };
                 match v {
                     Some(v) => {
-                        processed.insert(u.clone());
+                        processeds.insert(u.clone());
                         stack.push((u, neighbors));
-                        processed.insert(v.clone());
-                        let mut tmp_neighbors = f(&v, &processed, data)?;
+                        processeds.insert(v.clone());
+                        let mut tmp_neighbors = f(&v, &processeds, data)?;
                         tmp_neighbors.reverse();
                         stack.push((v.clone(), tmp_neighbors));
-                        visited.insert(v.clone());
+                        visiteds.insert(v.clone());
                     },
                     None => g(&u, data)?,
                 }
@@ -51,7 +51,7 @@ pub fn dfs_with_result<T: Clone + Ord, U, E, F, G>(from: &T, visited: &mut BTree
     Ok(())
 }
 
-pub fn dfs<T: Clone + Ord, U, F, G>(from: &T, visited: &mut BTreeSet<T>, data: &mut U, mut f: F, mut g: G)
+pub fn dfs<T: Clone + Ord, U, F, G>(from: &T, visiteds: &mut BTreeSet<T>, data: &mut U, mut f: F, mut g: G)
     where F: FnMut(&T, &BTreeSet<T>, &mut U) -> Vec<T>,
         G: FnMut(&T, &mut U)
-{ let _res: Result<(), ()> = dfs_with_result(from, visited, data, |u, processed, data| Ok(f(u, processed, data)), |u, data| Ok(g(u, data))); }
+{ let _res: Result<(), ()> = dfs_with_result(from, visiteds, data, |u, processeds, data| Ok(f(u, processeds, data)), |u, data| Ok(g(u, data))); }
